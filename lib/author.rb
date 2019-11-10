b5487014811b0681eca5746349a258d80f6c6895
Brandon Weaver <a7xncob@gmail.com>
2019-11-10 06:21:06 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,8 @@
 class Author
   attr_accessor :name
   
+  @@count = 0
+  
   def initialize(name)
     @name = name
     @posts = []
@@ -8,5 +10,19 @@ class Author
   
   def posts
     return Post.all.select { |post| post.author == self }
+  end
+  
+  def add_post(post)
+    @posts << post
+    post.author = self
+  end
+  
+  def add_post_by_title(title)
+    post = Post.new(title)
+    add_post(post)
+  end
+  
+  def self.post_count
+    return @@count
   end
 end
